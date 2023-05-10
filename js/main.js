d8d869aa8d949851fb003f857996065dc909fabb
hydrae-soft <torres.lf21@gmail.com>
2023-05-09 23:26:16 -0300

    Finished: mobile view.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('header')
+const header_menu = document.querySelector('#header-menu')
+
+const btn_menu = document.querySelector('#btn-menu')
+btn_menu.addEventListener('click', on_btn_menu_click)
+
+function on_btn_menu_click() {
+    header.classList.toggle('expand')
+    header_menu.classList.toggle('expand')
+    btn_menu.classList.toggle('expand')
+}
